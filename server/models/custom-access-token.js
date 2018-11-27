1361ecb46a12639ae213cc6e6c008fea7bc36c1f
Zelalem <zola@programmer.net>
2018-11-27 08:26:32 +0300

    proper relation between custom access token and custom user

--- server/models/custom-access-token.js
+++ server/models/custom-access-token.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(Customaccesstoken) {
-
+module.exports = function(CustomAccessToken) {
+    CustomAccessToken.observe("before save", async ctx => {
+        if (ctx.instance && ctx.isNewInstance) {
+            const { UserAccount } = CustomAccessToken.app.models;
+            try {
+                const user = await UserAccount.findById(ctx.instance.userId);
+                if (user) {
+                    delete user.password;
+                    ctx.instance.userInfo = user;
+                }
+            } catch (error) {
+                throw error;
+            }
+        }
+    });
 };
