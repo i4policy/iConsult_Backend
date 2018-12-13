14b28b65fd0be60344fa613cc96f0b02ebc73528
Zelalem <zola@programmer.net>
2018-12-13 14:51:02 +0300

    average rating

--- server/handlers/stats.js
+++ server/handlers/stats.js
@@ -1,57 +1,73 @@
 module.exports = function(Document) {
     return async function(documentId, options) {
-
+        
         let data = {
             sections: {}
         };
-
+        
         const { Section, Annotation } = Document.app.models;
-
-        const { Comment, Review } = Section.app.models;
-
+        
+        const { Comment, Review, Rate } = Section.app.models;
+        
         let sections = await Section.find({
             where: {
                 documentId: documentId
             }
         });
-
+        
         let annotations = await Annotation.find({
             where: {
                 documentId: documentId
             }
         });
-
+        
         data["totalAnnotations"] = annotations.length;
-
+        
         for (let i in sections) {
-
+            
             let section = sections[i];
-
+            
             let rc = await Review.find({
                 where: {
                     sectionId: section.id
                 }
             });
-
+            
             let cc = await Comment.find({
                 where: {
                     sectionId: section.id
                 }
             });
+            
+            let rac = await Rate.find({
+                where: {
+                    sectionId: section.id
+                }
+            });
 
+            let totalRating = rac.reduce((accumlator, value) => {
+                return accumlator + value.content;
+            }, 0);
+            
             rc = rc.length;
-
+            
             cc = cc.length;
 
+            rac = rac.length;
+
+            let averageRating = Math.round(totalRating / rac);
+            
             data.sections[section.title] = {
                 reviews: rc,
                 comments: cc,
-                totalEngagement: rc + cc
+                ratings: rac,
+                averageRating: averageRating,
+                totalEngagement: rc + cc + rac
             };
-
+            
         }
-
+        
         return data;
-
+        
     }
 };
