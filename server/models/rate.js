5fcb56f190ad9efdbb6ae1ca3ed38a6ae8cc888d
Zelalem <zola@programmer.net>
2018-12-13 14:14:06 +0300

    imporve semantics

--- server/models/rate.js
+++ server/models/rate.js
@@ -1,5 +1,17 @@
 'use strict';
 
 module.exports = function(Rate) {
-
+    
+    Rate.observe("before save", (context, next) => {
+        
+        if (context.instance) {
+            
+            context.instance.userId = context.options.accessToken.userId;
+            
+        }
+        
+        next();
+        
+    });
+    
 };
